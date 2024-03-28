d30d4da95170732bf021b7580556d66f2ac60350
JXgroup <113625623+JXgurub@users.noreply.github.com>
2024-03-28 10:26:24 +0500

    yangilanish-2

---
+++ engine/features.py
@@ -0,0 +1,10 @@
+from playsound import playsound
+
+
+
+
+
+# playing assiatnt sound function
+def playAssistantSound():
+    music_dir = "www\\assets\\audio\\start_sound.mp3"
+    playsound(music_dir)
