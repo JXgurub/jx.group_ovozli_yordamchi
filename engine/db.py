5391e39f405460ee5988c6400b14398c51d57d60
JXgroup <113625623+JXgurub@users.noreply.github.com>
2024-03-30 08:59:37 +0500

    .

--- engine/db.py
+++ engine/db.py
@@ -8,12 +8,12 @@ cursor = conn.cursor()
 
 
 
-query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
-cursor.execute(query)
+#query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key, name VARCHAR(100), path VARCHAR(1000))"
+#cursor.execute(query)
 
 
 
-#query = "INSERT INTO sys_command VALUES (null,'Telegram', 'D:\\telegram\\Telegram Desktop\\Telegram.exe')"
+#query = "INSERT INTO sys_command VALUES (null,'android studio', 'C:\\Program Files\\Android\\Android Studio\\bin\\studio64.exe')"
 #cursor.execute(query)
 #conn.commit()
 
@@ -21,8 +21,8 @@ cursor.execute(query)
 
 
 
-query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
-cursor.execute(query)
+#query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key, name VARCHAR(100), url VARCHAR(1000))"
+#cursor.execute(query)
 
 #query = "INSERT INTO web_command VALUES (null,'instagram', 'https://www.instagram.com/')"
 #cursor.execute(query)
@@ -30,6 +30,13 @@ cursor.execute(query)
 
 
 
+
+# testing module 
+
+app_name = "Telegram"
+cursor.execute('SELECT path FROM sys_command WHERE name IN (?)', (app_name,))
+results = cursor.fetchall()
+print(results[0][0])
 
 
 
